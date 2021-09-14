6380ce01585802d6437f943d3e9056f761f8b9d3
Lucas Henrique <lucashenrik02@gmail.com>
2021-09-14 11:23:42 -0300

    Criaçao de prototipos de telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,58 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TooltipModule} from 'primeng/tooltip';
+import {SidebarModule} from 'primeng/sidebar';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {CalendarModule} from 'primeng/calendar';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {DropdownModule} from 'primeng/dropdown';
+import { FormsModule }   from '@angular/forms';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+
+
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import { NavbarTesteComponent } from './navbar-teste/navbar-teste.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    NavbarTesteComponent,
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    BrowserAnimationsModule,
+    TooltipModule,
+    SidebarModule,
+    SelectButtonModule,
+    CalendarModule,
+    MessagesModule,
+    MessageModule,
+    DropdownModule,
+    FormsModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar-teste/navbar-teste.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-teste',
+  templateUrl: './navbar-teste.component.html',
+  styleUrls: ['./navbar-teste.component.css']
+})
+export class NavbarTesteComponent{
+
+  visibleSidebar1: any;
+
+  visibleSidebar2: any;
+
+  visibleSidebar3: any;
+
+  visibleSidebar4: any;
+
+  visibleSidebar5: any;
+
+}
